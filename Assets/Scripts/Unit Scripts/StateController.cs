7979c2e9521e849ca5e251a6722752905b953a32
Zachary Faulkner <zackfaulk247@gmail.com>
2025-06-25 12:21:12 -0500

    Added some more towers, rearranged some stuff, added a new StateController script

---
+++ Assets/Scripts/Unit Scripts/StateController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour
+{
+    public static StateController instance;
+
+    [SerializeField] public Sprite Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
